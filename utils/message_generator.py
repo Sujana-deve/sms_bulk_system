44f5116e54bcc5a86058f58d41f85df1aa856d4c
sujana <sharmasujana420@gmail.com>
2026-05-05 13:00:02 +0545

    minor change in message

--- utils/message_generator.py
+++ utils/message_generator.py
@@ -2,7 +2,7 @@ def generate_message(name, business_name):
     message = (
         f"Dear {name}, "
         f"Exciting offers are now available for {business_name}! "
-        f"Call us at 9800000000 or visit our website "
+        f"Call us at 9811111111 or visit our website "
         f"to learn more. Reply STOP to unsubscribe."
     )
     return message
